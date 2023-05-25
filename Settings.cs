c7b04c8563dc83c477333297372f7378eeb64c6a
Cesar Alvarez Doval <cesara@microsoft.com>
2023-05-25 14:57:31 +0200

    First commit

---
+++ Settings.cs
@@ -0,0 +1,10 @@
+namespace TestAppConfig
+{
+    public class Settings
+    {
+        public string BackgroundColor { get; set; } = "White";
+        public long FontSize { get; set; } = 20;
+        public string FontColor { get; set; } = "Black";
+        public string Message { get; set; } = "Initial Message";
+    }
+}
